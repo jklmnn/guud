63aacd14e79ce62a9e02152e9392043b41b09691
Johannes Kliemann <jk@jkliemann.de>
2017-11-17 16:43:25 +0100

    r/w 16 bit register, CP2108 GET_LINE_CTL bug test

--- src/server/uu_log/drivers/udrv_cp210x.cc
+++ src/server/uu_log/drivers/udrv_cp210x.cc
@@ -10,3 +10,41 @@ Genode::size_t Guud::Cp210x::tx(const char *line)
 {
     return 0;
 }
+
+bool Guud::Cp210x::valid_device(Genode::uint16_t vendor, Genode::uint16_t device)
+{
+    return vendor == 0x10c4 && device == 0xea60;
+}
+
+bool Guud::Cp210x::initialize()
+{
+    Genode::log(__func__);
+    Genode::uint16_t save = 0, test = 0;
+    Usb::Interface &iface = _device.interface(0);
+    iface.claim();
+    read_reg_16(iface, 4, &save);
+    Genode::log("saved value ", Genode::Hex(save));
+    write_reg_16(iface, 3, 0x800);
+    read_reg_16(iface, 4, &test);
+    Genode::log("tested value ", Genode::Hex(test));
+    write_reg_16(iface, 3, save);
+    return true;
+}
+
+void Guud::Cp210x::write_reg_16(Usb::Interface &iface, Genode::uint8_t reg, Genode::uint16_t value, Genode::uint16_t index)
+{
+    Usb::Packet_descriptor packet = iface.alloc(0);
+    Genode::uint8_t type = Usb::ENDPOINT_OUT | Usb::TYPE_VENDOR | Usb::RECIPIENT_DEVICE;
+    iface.control_transfer(packet, type, reg, value, index, 100);
+    iface.release(packet);
+}
+
+void Guud::Cp210x::read_reg_16(Usb::Interface &iface, Genode::uint8_t reg, Genode::uint16_t *value, Genode::uint16_t index)
+{
+    Usb::Packet_descriptor packet = iface.alloc(2);
+    Genode::uint8_t type = Usb::ENDPOINT_IN | Usb::TYPE_VENDOR | Usb::RECIPIENT_DEVICE;
+    iface.control_transfer(packet, type, reg, 0, index, 100);
+    *value = *(Genode::uint16_t*)iface.content(packet);
+    iface.release(packet);
+}
+
--- src/server/uu_log/include/drivers/cp210x.h
+++ src/server/uu_log/include/drivers/cp210x.h
@@ -10,6 +10,14 @@ namespace Guud {
 
 class Guud::Cp210x : public Guud::Uart
 {
+    private:
+
+        bool valid_device(Genode::uint16_t, Genode::uint16_t) override;
+        bool initialize() override;
+
+        void write_reg_16(Usb::Interface&, Genode::uint8_t, Genode::uint16_t, Genode::uint16_t = 0);
+        void read_reg_16(Usb::Interface&, Genode::uint8_t, Genode::uint16_t*, Genode::uint16_t = 0);
+
     public:
         Cp210x(Genode::Env &, Genode::Heap&, Genode::Signal_context_capability);
         
